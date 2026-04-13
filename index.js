88be59607b82a848a4ef2c458b7195f914559c23
Preetham Reddy <preetham.reddyyy8@gmail.com>
2026-04-13 15:57:59 +0530

    Setup the Task Lisk JSON File. Added add command functionality.

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-const args = process.argv.slice(2);
+const fs = require("fs");
 
-console.log("Arguments passed: ", args);
+const args = process.argv.slice(2);
+const command = args[0];
+if(!command) {
+    console.log("Please provide a valid command.");
+    process.exit(1);
+}
+const task_description = args[1];
+
+const task_list = fs.readFileSync("task-list.json", "utf-8");
+const data = JSON.parse(task_list);
+const tasks = data.tasks;
+
+// add
+if(command === "add") {
+
+    if(!task_description) {
+        console.log("Please provide a task description.");
+        process.exit(1);
+    }
+
+    const newTask = {
+        id : data.lastId + 1,
+        description : task_description,
+        status : "todo",
+        createdAt : new Date().toISOString(),
+        updatedAt : new Date().toISOString()
+    }
+    data.lastId = data.lastId + 1;
+
+    tasks.push(newTask);
+
+    fs.writeFileSync("task-list.json", JSON.stringify(data, null, 2), "utf-8");
+    console.log(`Task added successfully (ID : ${data.lastId})`);
+}
+
+// update
+
+// delete
+
+// mark-in-progress
+
+//mark-done
+
+// list
+
+// list done
+
+// list todo
+
+// list in-progress
+
+// console.log("Arguments passed: ", args);
